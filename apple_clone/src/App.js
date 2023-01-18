f78b8ddfa27a6d39b986e4305df6493b24065b4d
subrat199 <subratt01998@gmail.com>
2023-01-18 20:25:21 +0530

    step3

--- apple_clone/src/App.js
+++ apple_clone/src/App.js
@@ -9,6 +9,7 @@ function App() {
       <Navbar/>
       <AllRoutes/>
      <Introducing/>
+  
     </div>
   );
 }
--- apple_clone/src/Component/Introducing/Introducing.jsx
+++ apple_clone/src/Component/Introducing/Introducing.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import './Intoducing.css'
-import { Button } from '@chakra-ui/react';
-
+import { Button, Box, background } from '@chakra-ui/react';
+import { Image } from '@chakra-ui/react'
 const Introducing = () => {
   return (
     <div className='introducing'>
       <p class='intro'>Introducing the new <br /> MacBook Pro and Mac mini.</p>
       <Button  class='button' >
         <a href="https://www.youtube.com/watch?v=OushE7mq0Ak">Watch The Announcment</a>
+      </Button>
+   {/* <p class='mac'><a href="https://www.apple.com/macbook-pro-14-and-16/">MacBook Pro <br /></a></p> */}
+   <p style={{color:'white',fontSize:'large',marginTop:'50px'}}> <a href="https://www.apple.com/macbook-pro-14-and-16/">Supercharged by M2 Pro  and M2 Max</a> </p>
+   <p style={{color:'grey',fontSize:'larger',marginTop:'10px'}}> <a href="https://www.apple.com/macbook-pro-14-and-16/">Available Starting 24 Jan</a></p>
+   <Button colorScheme='black' variant='link' marginTop={'10px'} color={'lightblue'}>
+   Order Now
   </Button>
-   <p class='mac'><a href="https://www.apple.com/macbook-pro-14-and-16/">MacBook Pro</a></p>
-   
+  <Box boxSize='lr' bg={'black'}>
+  <Image src='https://www.apple.com/v/macbook-pro-14-and-16/d/images/overview/hero/hero_intro_endframe__e6khcva4hkeq_large.jpg' alt='Dan Abramov'  w={'60%'} margin={'auto'}/>
+  <p style={{color:'white',fontSize:'large',marginTop:'20px' }}> <a href="https://www.apple.com/macbook-pro-14-and-16/">Supercharged by M2 Pro  and M2 Max</a> </p>
+   <p style={{color:'grey',fontSize:'larger',marginTop:'10px'}}> <a href="https://www.apple.com/macbook-pro-14-and-16/">Available Starting 24 Jan</a></p>
+   <Button colorScheme='black' variant='link' marginTop={'10px'} color={'lightblue'}>
+   Order Now
+  </Button>
+  <Image src='https://9to5mac.com/wp-content/uploads/sites/6/2022/10/mac-mini-m2.jpg?quality=82&strip=all&w=960' alt='Dan Abramov'  w={'60%'} margin={'auto'} />
+  </Box>
     </div>
   )
 }
